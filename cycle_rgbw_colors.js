59d0205d921b3f153dfceafd404a4d80a39241c9
Thorsten Eurich <thorsten@eurich.de>
2021-10-16 07:51:19 +0200

    Added description and header

--- cycle_rgbw_colors.js
+++ cycle_rgbw_colors.js
@@ -1,9 +1,21 @@
- // this is the remote shelly which we want to control.
-
+ /**
+ * @copyright shelly-tools contributors
+ * @license   GNU Affero General Public License (https://www.gnu.org/licenses/agpl-3.0.de.html)
+ * @authors   https://github.com/shelly-tools/shelly-script-examples/graphs/contributors
+ *
+ * This script is intended to control a Shelly RGBW2 / ShellyDuo RGBW with a single button from
+ * a Shelly Plus device. Every time the button from the Plus device is pushed it jumps to the next step 
+ * defined in the sequence array. 
+ */
+ 
+// CONFIG START
+// IP address or hostname from RGBW device
  let REMOTE = {
      ip: '192.168.178.70',
  };
 
+// button sequence 
+// The demo sequence is: on and blue with max brightness, then red, green, yellow, purple, off
 let sequence = [
     '?turn=on&gain=100&blue=255&red=0&green=0', // blue
     '?turn=on&gain=100&blue=0&red=255&green=0', // red
@@ -12,7 +24,9 @@ let sequence = [
     '?turn=on&gain=100&blue=225&red=185&green=76', // purple
     '?turn=off' // off
 ];
- // no need to change anything below this line..
+// CONFIG END 
+
+// no need to change anything below this line..
 let position = 0;
 
 // add an evenHandler for button type input and single push events
