eb588aa5abca8b7ea3f997527a747e60a0bd814f
Thorsten Eurich <thorsten@eurich.de>
2021-10-16 08:16:49 +0200

    Added headers and description for monitoring scripts

--- monitor_local_consumption.js
+++ monitor_local_consumption.js
@@ -1,5 +1,14 @@
-// Example script to power-monitor a local connected appliance (washing machine or dryer) and send a push notification via Telegram once the appliance has  
-// finished the job.
+
+/**
+ * @copyright shelly-tools contributors
+ * @license   GNU Affero General Public License (https://www.gnu.org/licenses/agpl-3.0.de.html)
+ * @authors   https://github.com/shelly-tools/shelly-script-examples/graphs/contributors
+ *
+ * This script is intended to measure power consumption from the local device, e.g. a Shelly Plus 1PM,
+ * and send a telegram push notification once the power consumption stops for a given time period.
+ * the idea behind is to monitor a washing machine or dryer and send a notification once the appliance
+ * has finished.
+ */
 
 // CONFIG START
 
--- monitor_remote_consumption.js
+++ monitor_remote_consumption.js
@@ -1,3 +1,14 @@
+ /**
+ * @copyright shelly-tools contributors
+ * @license   GNU Affero General Public License (https://www.gnu.org/licenses/agpl-3.0.de.html)
+ * @authors   https://github.com/shelly-tools/shelly-script-examples/graphs/contributors
+ *
+ * This script is intended to measure power consumption of a remote Shelly Plug / Plug-S / Shelly1PM
+ * and send a telegram push notification once the power consumption stops for a given time period.
+ * the idea behind is to monitor a washing machine or dryer and send a notification once the appliance
+ * has finished.
+ */
+ 
 // CONFIG START
 // IP from the remote shelly plug / plug-s we'd like to monitor
 let remoteip = '192.168.178.205';
