251a8b06924972d19e70cb3f1d3a63af2fde4b99
Thorsten Eurich <thorsten@eurich.de>
2021-12-07 19:08:30 +0100

    update Cycle switch for I4 support.

--- emulate_shutter_cycleswitch.js
+++ emulate_shutter_cycleswitch.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * @copyright shelly-tools contributors
  * @license   GNU Affero General Public License (https://www.gnu.org/licenses/agpl-3.0.de.html)
  * @authors   https://github.com/shelly-tools/shelly-script-examples/graphs/contributors
@@ -11,66 +11,67 @@
  
 // CONFIG START
 // this is the remote shelly which we want to control.
+// Input is the number of the switch
 
- let REMOTE = {
-     ip: '192.168.178.209',
- };
+let CONFIG = {
+    ip: '192.168.178.209',
+    input: 0,
+    btnevent: 'single_push'
+};
 // CONFIG END
 
- // no need to change anything below this line..
+// no need to change anything below this line..
 
 // add an evenHandler for button type input and single push events
- Shelly.addEventHandler(
-     function (event, user_data) {
-         //print(JSON.stringify(event));
-         if (typeof event.info.event !== 'undefined') {
+Shelly.addEventHandler(
+    function (event, user_data) {
+        //print(JSON.stringify(event));
+        if (typeof event.info.event !== 'undefined') {
+            if (event.info.id === CONFIG.input && event.info.event === CONFIG.btnevent) {
+                getCurrentState(CONFIG.ip);
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    },
+);
 
-             if (event.info.event === 'single_push') {
-                 getCurrentState(REMOTE.ip);
-             } else {
-                 return true;
+// query a remote shelly and determine if next step is open, stop or close
+function getCurrentState(ip) {
+    Shelly.call(
+        "http.get", {
+            url: 'http://' + ip + '/roller/0'
+        },
+        function (res, error_code, error_message, ud) {
+            if (res.code === 200) {
+                let st = JSON.parse(res.body);
+                if (st.state === 'stop' && st.last_direction === 'close') {
+                    controlShutter(CONFIG.ip, 'open');
+                    print("open shutter");
+                } else if (st.state === 'stop' && st.last_direction === 'open') {
+                    controlShutter(CONFIG.ip, 'close');
+                    print("close shutter");
+                } else {
+                    controlShutter(CONFIG.ip, 'stop');
+                    print("stop shutter");
+                }
+            }
+        },
+        null
+    );
+};
 
-             }
-         } else {
-             return true;
-         }
-     },
- );
+// send shutter command 
+function controlShutter(ip, command) {
+    Shelly.call(
+        "http.get", {
+            url: 'http://' + ip + '/roller/0?go=' + command
+        },
+        function (response, error_code, error_message, ud) {
 
- // query a remote shelly and determine if next step is open, stop or close
- function getCurrentState(ip) {
-     Shelly.call(
-         "http.get", {
-             url: 'http://' + ip + '/roller/0'
-         },
-         function (res, error_code, error_message, ud) {
-             if (res.code === 200) {
-                 let st = JSON.parse(res.body);
-                 if (st.state === 'stop' && st.last_direction === 'close') {
-                     controlShutter(REMOTE.ip, 'open');
-                     print("open shutter");
-                 } else if (st.state === 'stop' && st.last_direction === 'open') {
-                     controlShutter(REMOTE.ip, 'close');
-                     print("close shutter");
-                 } else {
-                     controlShutter(REMOTE.ip, 'stop');
-                     print("stop shutter");
-                 }
-             }
-         },
-         null
-     );
- };
-
- // send shutter command 
- function controlShutter(ip, command) {
-     Shelly.call(
-         "http.get", {
-             url: 'http://' + ip + '/roller/0?go=' + command
-         },
-         function (response, error_code, error_message, ud) {
-             print(command);
-         },
-         null
-     );
- };
+        },
+        null
+    );
+};
