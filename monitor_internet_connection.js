c62372d98bc1d8d896086c1ce8dec23ce248ee84
Thorsten Eurich <thorsten@eurich.de>
2023-02-25 14:25:40 +0100

    cleanup and fixed a updated API command

--- monitor_internet_connection.js
+++ monitor_internet_connection.js
@@ -1,57 +1,42 @@
- /**
- * @copyright shelly-tools contributors
- * @license   GNU Affero General Public License (https://www.gnu.org/licenses/agpl-3.0.de.html)
- * @authors   https://github.com/shelly-tools/shelly-script-examples/graphs/contributors
- *
- * This script is intended to control a Shelly RGBW2 / ShellyDuo RGBW with a single button from
- * a Shelly Plus device. Every time the button from the Plus device is pushed it jumps to the next step 
- * defined in the sequence array. 
- */
- 
+/**
+* @copyright shelly-tools contributors
+* @license   GNU Affero General Public License (https://www.gnu.org/licenses/agpl-3.0.de.html)
+* @authors   https://github.com/shelly-tools/shelly-script-examples/graphs/contributors
+*
+* This script can be used to monitor the connection to the internet and restart the local relay
+* if the connection fails for a configurable time. Perfect use case would be a router restart.
+*/
+
 // CONFIG START
-// a remote URL with just a few bytes of content in order to check if internet is still available.
-let remoteurl = 'https://gist.githubusercontent.com/eurich/6e84e85c11401c4e28a2676492d846b7/raw/5d577bc2dfadfa13887f9b2ec146fe1b2ee2f5b6/gistfile1.txt';
-// number of times the check is done before internet is considered as down.
-let maxfails = 5;
-// checks the internet connection every x minutes, recommended is 5 or more
-let interval = 5;
+let CONFIG = {
+    // a remote URL with just a few bytes of content in order to check if internet is still available.
+    remoteurl: 'https://global.gcping.com/ping',
+    maxfails: 5, // number of times the check is done before internet is considered as down.
+    interval: 5, // checks the internet connection every x minutes, recommended is 5 or more
+}
 // CONFIG END
 
-// no need to change anything below this line.
+// internal vars and functions, no need to change anything below this line.
 let alertTimer = '';
 let failcounter = 0;
-function startMonitor() {
-    alertTimer = Timer.set(interval *60 * 1000,
-        true,
-        function () {
-            Shelly.call("HTTP.GET", {
-                    url: remoteurl
-                },
-                function (res, error_code, error_msg, ud) {
-                    if (error_code !== 0) {                  
-                       if (failcounter === maxfails) {
-                            print("Restart");
-                            restartRelay();
-                            failcounter = 0;
-                        } else {
-                           print("fail");
-                            failcounter++;
-                        }
-                    }
-                },
-                null
-            );
-        },
-        null
-    );
-}
 
-function restartRelay() {
-    Shelly.call(
-        "switch.set",
-        { id: 0, on: false, toggle: 2},
-        function (result, code, msg, ud) {
+function startMonitor() {
+    alertTimer = Timer.set(CONFIG.interval * 60 * 1000, true, function () {
+        Shelly.call("HTTP.GET", {
+            url: CONFIG.remoteurl
+        }, function (res, error_code, error_msg, ud) {
+            if (error_code !== 0) {
+                if (failcounter === CONFIG.maxfails) {
+                    Shelly.call("Switch.set", { id: 0, on: false, toggle_after: 2 });
+                    failcounter = 0;
+                } else {
+                    failcounter++;
+                }
+            }
         },
+            null
+        );
+    },
         null
     );
 }
